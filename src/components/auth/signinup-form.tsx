f264689b8f0bc7ca94a61af281d6bcbd8f805bdc
WiliamDev <wiliam.omura3@hotmail.com>
2026-05-15 01:17:09 -0300

    fix signup

--- src/components/auth/signinup-form.tsx
+++ src/components/auth/signinup-form.tsx
@@ -1,50 +1,47 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+
 import { useState } from "react";
 
 import { Input } from "../ui/input";
+
 import { Button } from "../ui/button";
 
 export const SignUpForm = () => {
-
   const router = useRouter();
 
   const [nameField, setNameField] = useState("");
+
   const [emailField, setEmailField] = useState("");
+
   const [passwordField, setPasswordField] = useState("");
 
   const handleEnterButton = async () => {
-
     try {
+      const res = await fetch("http://127.0.0.1:8000/api/users/create/", {
+        method: "POST",
 
-      const res = await fetch(
-        "http://127.0.0.1:8000/api/users/create/",
-        {
-          method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
 
-          headers: {
-            "Content-Type": "application/json",
-          },
-
-          body: JSON.stringify({
-            username: nameField,
-            email: emailField,
-            password: passwordField,
-          }),
-        }
-      );
+        body: JSON.stringify({
+          username: nameField,
+          email: emailField,
+          password: passwordField,
+        }),
+      });
 
       const data = await res.json();
 
       if (!res.ok) {
-
         alert(data.error);
-        return;
 
+        return;
       }
 
-      // 🔥 SALVA NOVO USUÁRIO
+      // SALVA NOVO USUÁRIO
       localStorage.setItem(
         "user_data",
 
@@ -58,53 +55,44 @@ export const SignUpForm = () => {
 
           avatar: "",
           cover: "",
-        })
+        }),
       );
 
       // ENTRA DIRETO
       router.replace("/home");
-
     } catch (err) {
-
       console.error(err);
 
       alert("Erro ao criar conta");
-
     }
-
   };
 
   return (
-
     <div className="flex flex-col gap-4">
-
+      {/* NOME */}
       <Input
         placeholder="Digite seu nome"
         value={nameField}
         onChange={(t) => setNameField(t)}
       />
 
+      {/* EMAIL */}
       <Input
         placeholder="Digite seu e-mail"
         value={emailField}
         onChange={(t) => setEmailField(t)}
       />
 
+      {/* SENHA */}
       <Input
-        type="password"
+        isPassword
         placeholder="Digite sua senha"
         value={passwordField}
         onChange={(t) => setPasswordField(t)}
       />
 
-      <Button
-        label="Criar conta"
-        onClick={handleEnterButton}
-        size={1}
-      />
-
+      {/* BOTÃO */}
+      <Button label="Criar conta" onClick={handleEnterButton} size={1} />
     </div>
-
   );
-
 };
